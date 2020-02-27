c87e59acc0229b9a277c81ad68a70a8e01a9a002
Anna Musiewicz <annamusiewicz@gmail.com>
2020-02-27 09:48:53 +0100

    Task 3.3 hotfix

--- src/main/java/RandomNumbers.java
+++ src/main/java/RandomNumbers.java
@@ -7,6 +7,15 @@ public class RandomNumbers {
     private int minNumber;
     private List<Integer> numbers = new ArrayList<>();
 
+    public static void main(String[] args) {
+
+        RandomNumbers randomNumbers = new RandomNumbers();
+        randomNumbers.generate();
+        int max = randomNumbers.getMaxNumber();
+        int min = randomNumbers.getMinNumber();
+        System.out.println("The min random number is " + min + ", the max random number is " + max);
+    }
+
     public int getMaxNumber() {
         return maxNumber;
     }
@@ -21,37 +30,36 @@ public class RandomNumbers {
 
     public void generate() {
         Random random = new Random();
-        int sum = 0;
-        maxNumber = Integer.MIN_VALUE;
-        minNumber = Integer.MAX_VALUE;
-        int number;
+
+        int firstNumber = random.nextInt(31);
+        System.out.println("The first random number is " + firstNumber);
+
+        numbers.add(firstNumber);
+        maxNumber = firstNumber;
+        minNumber = firstNumber;
+
+        int sum = firstNumber;
 
         while (sum <= 5000) {
-            number = random.nextInt(31);
+           int number = random.nextInt(31);
+            System.out.println("The next random number is " + number);
             numbers.add(number);
 
             if (number > maxNumber) {
+                System.out.println("The previous max number is " + maxNumber + ", the new max number is " + number );
+
                 maxNumber = number;
             }
 
             if (number < minNumber) {
+                System.out.println("The previous min number is " + minNumber + ", the new min number is " + number );
+
                 minNumber = number;
             }
 
             sum = sum + number;
+            System.out.println(" The sum is: " + sum);
         }
-    }
-
-    public String getStatus() {
-        return "min: " + minNumber + ", max: " + maxNumber + ", numbers: " + numbers;
-    }
-
-    public static void main(String[] args) {
-
-        RandomNumbers randomNumbers = new RandomNumbers();
-        randomNumbers.generate();
-        System.out.println(randomNumbers.getStatus());
-
     }
 }
 
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,26 +1,19 @@
 public class User {
-String name;
-int age;
+    String name;
+    int age;
 
-public User(String name, int age) {
-    this.name = name;
-    this.age = age;
-}
-    public String getName() {
-        return this.name;
-    }
-
-    public int getAge() {
-        return this.age;
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public static void main(String[] args) {
 
-    User anna = new User("Anna", 20);
-    User marta = new User("Marta", 36);
-    User nel = new User("Nel", 33);
-    User viki = new User("Viki", 18);
-    User suzie = new User("Suzie", 28);
+        User anna = new User("Anna", 20);
+        User marta = new User("Marta", 36);
+        User nel = new User("Nel", 33);
+        User viki = new User("Viki", 18);
+        User suzie = new User("Suzie", 28);
 
         User[] users = {anna, marta, nel, viki, suzie};
 
@@ -35,9 +28,17 @@ public User(String name, int age) {
         System.out.println("The average age of user is  " + averageAge);
 
         for (int i = 0; i < users.length; i++) {
-            if ( users[i].age < averageAge) {
-                System.out.println( " Users under the average age' user :" + users[i].name);
+            if (users[i].age < averageAge) {
+                System.out.println(" Users under the average age' user :" + users[i].name);
             }
         }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
     }
 }
