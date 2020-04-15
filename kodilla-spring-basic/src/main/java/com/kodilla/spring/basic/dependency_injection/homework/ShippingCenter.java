99746fb4ce7a72c5809b40c958a8d98a555241c7
Anna Musiewicz <annamusiewicz@gmail.com>
2020-04-15 22:34:31 +0200

    Task 9.1 DeliveryService

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/ShippingCenter.java
@@ -0,0 +1,23 @@
+package com.kodilla.spring.basic.dependency_injection.homework;
+
+public class ShippingCenter {
+
+    private DeliveryService deliveryService;
+    private NotificationService notificationService;
+
+    public ShippingCenter(DeliveryService deliveryService, NotificationService notificationService) {
+        this.deliveryService = deliveryService;
+        this.notificationService = notificationService;
+    }
+
+//    private DeliveryService deliveryService = new DeliveryService();
+//    private NotificationService notificationService = new NotificationService();
+
+    public void sendPackage(String address, double weight) {
+        if (deliveryService.deliverPackage(address, weight)) {
+            notificationService.success(address);
+        }
+        else
+        notificationService.fail(address);
+    }
+}
