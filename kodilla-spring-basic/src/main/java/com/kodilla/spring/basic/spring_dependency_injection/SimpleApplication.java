57efcf9e08a10942e1858eb3969e9044c579296b
Anna Musiewicz <annamusiewicz@gmail.com>
2020-04-18 01:04:13 +0200

    MessageService Spring Injections Part 9.3

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
@@ -1,13 +1,26 @@
 package com.kodilla.spring.basic.spring_dependency_injection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleApplication {
 
+    //utworzylismy interfejs i teraz konstruktor klasy SA go przejmuje
+    // jednak w ten sposób powstały 4 beany typu MessageService i Spring nie wie którego uzyć
+    //dlatego wracamy do wstrzykiwania zależności przez pole
+//    private MessageService messageService;
+//
+//    public SimpleApplication(MessageService messageService) {
+//        this.messageService = messageService;
+//    }
+
     //wstrzykiwanie przez pole
-   // @Autowired
+    //musimy wskazac dokładnie jaki obiekt wstrzykujemy
+    @Autowired
+    @Qualifier(value = "skypeMessageService")
+    //@Resource(name = "skypeMessageService")
     private SkypeMessageService messageService;
 
 
@@ -17,10 +30,10 @@ public class SimpleApplication {
 //    }
 
     // wstrzykiwanie przez setter
-    @Autowired
-    public void setMessageService(SkypeMessageService messageService) {
-        this.messageService = messageService;
-    }
+//    @Autowired
+//    public void setMessageService(SkypeMessageService messageService) {
+//        this.messageService = messageService;
+//    }
 
     public String processMessage(String message, String receiver) {
         if (checkReceiver(receiver)) {
