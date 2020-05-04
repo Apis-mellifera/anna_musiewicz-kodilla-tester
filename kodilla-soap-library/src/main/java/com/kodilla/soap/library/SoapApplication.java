f4648bd0d210bd39f520322fc2d0f61fc312a00f
Anna Musiewicz <annamusiewicz@gmail.com>
2020-05-04 18:30:42 +0200

    Task 11.2 SOAP Library

---
+++ kodilla-soap-library/src/main/java/com/kodilla/soap/library/SoapApplication.java
@@ -0,0 +1,11 @@
+package com.kodilla.soap.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SoapApplication.class, args);
+    }
+}
---
+++ kodilla-soap-library/src/main/java/com/kodilla/soap/library/repository/BooksRepository.java
@@ -0,0 +1,59 @@
+package com.kodilla.soap.library.repository;
+
+import com.kodilla.books.soap.Book;
+import com.kodilla.books.soap.Genre;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class BooksRepository {
+    private static final Map<String, Book> books = new HashMap<>();
+
+    @PostConstruct
+    public void initData() {
+        Book hobbit = new Book();
+        hobbit.setCallNo("123FS");
+        hobbit.setTitle("The Hobbit");
+        hobbit.setAuthor("J.R.R. Tolkien");
+        hobbit.setPubYear(1937);
+        hobbit.setGenre(Genre.FANTASY);
+
+        books.put(hobbit.getCallNo(), hobbit);
+
+        Book endlessNight = new Book();
+        endlessNight.setCallNo("456CRI");
+        endlessNight.setTitle("Endless Night");
+        endlessNight.setAuthor("A. Cristie");
+        endlessNight.setPubYear(1967);
+        endlessNight.setGenre(Genre.CRIME);
+
+        books.put(endlessNight.getCallNo(), endlessNight);
+
+        Book ubik = new Book();
+        ubik.setCallNo("789SF");
+        ubik.setTitle("Ubik");
+        ubik.setAuthor("P.K. Dick");
+        ubik.setPubYear(1975);
+        ubik.setGenre(Genre.SCI_FI);
+
+        books.put(ubik.getCallNo(), ubik);
+
+        Book howSmart = new Book();
+        howSmart.setCallNo("908TE");
+        ubik.setTitle("How smart machines think");
+        ubik.setAuthor("S. Gerrish");
+        ubik.setPubYear(2019);
+        ubik.setGenre(Genre.TECH);
+
+        books.put(howSmart.getCallNo(), howSmart);
+    }
+
+    public Book findBook(String callNo) {
+        Assert.notNull(callNo, "You have to specify the books call number");
+        return books.get(callNo);
+    }
+}
