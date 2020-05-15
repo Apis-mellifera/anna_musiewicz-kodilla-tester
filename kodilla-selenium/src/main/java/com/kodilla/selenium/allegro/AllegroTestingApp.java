5d5b70c00959efd897a48945e70ea65e6c9efe0b
Anna Musiewicz <annamusiewicz@gmail.com>
2020-05-15 21:25:17 +0200

    Task 13.4 Selenium Allegro

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -0,0 +1,45 @@
+package com.kodilla.selenium.allegro;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class AllegroTestingApp {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://allegro.pl/");
+
+       // while (!driver.findElement(By.xpath("//html/body/div/div/div/div/div/button/img")).isDisplayed()) {
+       //     driver.findElement(By.xpath("//html/body/div/div/div/div/div/button/img")).click();
+       // }
+
+        while (!driver.findElement(By.xpath("//*[@data-box-name=\"allegro.rodoConsentModal\"]/div/div/div/button")).isDisplayed()) {
+        }
+        driver.findElement(By.xpath("//*[@data-box-name=\"allegro.rodoConsentModal\"]/div/div/div/button")).click();
+
+        WebElement inputField = driver.findElement(
+                By.xpath("//*[@name=\"string\"]")
+        );
+        inputField.sendKeys("mavic mini");
+        inputField.submit();
+
+        WebElement electronicsCombo = driver.findElement(
+                By.xpath("//*[@data-role=\"header-wrapper\"]/div/div/div/div/form/div/div/select[1]")
+        );
+        Select electronicsSelect = new Select(electronicsCombo);
+        electronicsSelect.selectByIndex(3);
+
+        WebElement searchButton = driver.findElement(
+                By.xpath("//*[@data-role=\"search-button\"]")
+        );
+        searchButton.click();
+
+
+
+
+    }
+}
