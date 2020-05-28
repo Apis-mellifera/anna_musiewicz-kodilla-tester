b2737f6bfd9d490bcc7d5243fcaa9a82d1af788b
Anna Musiewicz <annamusiewicz@gmail.com>
2020-05-28 23:54:41 +0200

    Task 15.4 Hibernate Invoice

---
+++ kodilla-code-coverage/src/main/java/com/kodilla/hibernate/invoice/Item.java
@@ -0,0 +1,91 @@
+package com.kodilla.hibernate.invoice;
+
+// Celem zadania jest stworzenie encji reprezentującej nagłówki faktur, a także pozycje faktury.
+// Pozycje będą posiadały również relację do listy towarów.
+// klasa reprezentuje pozycję faktury
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "ITEMS")
+public class Item {
+    private int id;
+    private BigDecimal price;
+    private int quantity;
+    private BigDecimal value;
+    // pole product przechowuje informacje do którego produktu należą pozycje faktury
+    private Product product;
+    // pole invoice przechowuje informacje do jakiego nagłówka należą pozycje faktury
+    private Invoice invoice;
+
+    public Item() {
+
+    }
+
+    public Item(BigDecimal price, int quantity, BigDecimal value) {
+        this.price = price;
+        this.quantity = quantity;
+        this.value = value;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @Column(name = "PRICE")
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    @Column(name = "QUANTITY")
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Column(name = "VALUE")
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    private void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    private void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "PRODUCT_ID")
+    public Product getProduct() {
+        return product;
+    }
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "INVOICE_ID")
+    public Invoice getInvoice() {
+        return invoice;
+    }
+
+    public void setInvoice(Invoice invoice) {
+        this.invoice = invoice;
+    }
+}
---
+++ kodilla-code-coverage/src/test/java/com/kodilla/hibernate/invoice/repository/InvoiceRepositoryTestSuite.java
@@ -0,0 +1,49 @@
+package com.kodilla.hibernate.invoice.repository;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceRepositoryTestSuite {
+
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+
+    // test ma sprawdzać czy faktura z kilkoma pozycjami zapisuje się poprawnie w bazie danych
+    @Test
+    public void testInvoiceRepositorySave() {
+        //given
+        Item item = new Item(new BigDecimal(30), 10, new BigDecimal(50));
+        Item item2 = new Item(new BigDecimal(40), 20, new BigDecimal(60));
+        Product product = new Product("Softshell jacket");
+        Invoice invoice = new Invoice("FV2345/01/20");
+        product.getItems().add(item);
+        product.getItems().add(item2);
+        item.setProduct(product);
+        item2.setProduct(product);
+        invoice.getItems().add(item);
+        invoice.getItems().add(item2);
+        item.setInvoice(invoice);
+        item2.setInvoice(invoice);
+
+        //when
+        invoiceRepository.save(invoice);
+        int id = invoice.getId();
+
+        //then
+        Assert.assertNotEquals(0, id);
+
+        //cleanUp
+        invoiceRepository.deleteById(id);
+    }
+}
